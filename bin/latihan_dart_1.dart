6eae497ca85e44ffc70566bd766ea43630a6ec13
I Ketut Gunawan <iketut.gunawan@vida.id>
2025-09-28 09:27:08 +0700

    perubaan code variable

--- bin/latihan_dart_1.dart
+++ bin/latihan_dart_1.dart
@@ -1,3 +1,13 @@
 void main() {
   print('Hello world !!! latihan dart');
+
+  var fullName = 'Dart Programming';
+  var age = 1;
+  var height = 1.5;
+  var isMarried = true;
+
+  print(fullName);
+  print(age);
+  print(isMarried);
+  print(height);
 }
