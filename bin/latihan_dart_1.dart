2799ce5f23ab5f5bf28f4ee2a0aac2247a405424
I Ketut Gunawan <iketut.gunawan@vida.id>
2025-09-28 09:03:50 +0700

    remove library, update main hello world

--- bin/latihan_dart_1.dart
+++ bin/latihan_dart_1.dart
@@ -1,5 +1,3 @@
-import 'package:latihan_dart_1/latihan_dart_1.dart' as latihan_dart_1;
-
-void main(List<String> arguments) {
-  print('Hello world: ${latihan_dart_1.calculate()}!');
+void main() {
+  print('Hello world !!! latihan dart');
 }
